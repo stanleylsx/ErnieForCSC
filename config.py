cdd612bd9fdf9da80b7d3ce8367e6cd3a5009327
lishouxian <gzlishouxian@gmail.com>
2022-08-10 15:59:42 +0800

    Update config.py

--- config.py
+++ config.py
@@ -18,6 +18,15 @@ configure = {
     'max_sequence_length': 128,
     'learning_rate': 5e-5,
     'epochs': 5,
-    'batch_size': 16,
-    'model_name': 'ernie4csc'
+    'batch_size': 32,
+    'model_name': 'ernie4csc.pkl',
+    # 是否进行warmup
+    'warmup': True,
+    # warmup方法，可选：linear、cosine
+    'scheduler_type': 'linear',
+    # warmup步数，-1自动推断为总步数的0.1
+    'num_warmup_steps': -1,
+    'print_per_batch': 100,
+    'is_early_stop': True,
+    'patient': 3,
 }
